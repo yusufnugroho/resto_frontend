a189fc6e261c28601950ac984c69790ed4a2d294
deadianti <deadianti10@gmail.com>
2018-01-27 22:23:08 +0700

    resto update

--- src/pages/home/homepage.js
+++ src/pages/home/homepage.js
@@ -3,7 +3,24 @@ import {observer, inject} from 'mobx-react';
 class Homepage extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      all_menu: []
+    }
     this.moveToOrderPage = this.moveToOrderPage.bind(this)
+  }
+  componentDidMount(){
+    console.log(this.state.all_menu)
+    const allMenuFromDB = fetch("http://localhost:9000/menus",{
+      method: 'GET',
+    }).then(res => res.json()).then((res)=>{
+      console.log("Current Result ", res)
+      this.setState({
+        all_menu: res
+      })
+      console.log(this.state.all_menu)
+    })
+    console.log(allMenuFromDB)
+    console.log("COMPONENT DID MOUNT DIPANGGIL")
   }
   moveToOrderPage(){
     this.props.history.push("/order");
